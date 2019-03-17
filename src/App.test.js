84d0970381d6ad424f5a01cff63b146970693f0a
Christoph <ignatz@gmail.com>
2019-03-17 14:14:43 -0400

    added enzyme and setup

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {shallow} from 'enzyme';
 import App from './App';
+import { isExportDeclaration } from 'typescript';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+});
+
+it('renders a div with that className prop set to be an item card', () => {
+  const wrapper = shallow(<App />);
+
+  // expect(false).toBe(true);
+  expect(wrapper.find('div').findWhere(e => e.props().className === )).to.have.lengthOf(1);
+  // wrapper.find('div').findWhere(e => e.props().className === 'list-item-card')
 });
